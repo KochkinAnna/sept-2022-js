151c08d436a804277fb8b1f572338489dce5d652
KochkinAnna <kochkinaanichka@gmail.com>
2022-11-01 11:50:20 +0200

    without promt

---
+++ lessson1_js/main.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let thefence = 'hello';
+console.log(thefence);
+
+thefence = 'owu';
+console.log(thefence);
+
+thefence = 'com';
+console.log(thefence);
+
+thefence = 'ua';
+console.log(thefence);
+
+let num = 1;
+console.log(num);
+
+num = 10;
+console.log(num);
+
+num = -999;
+console.log(num);
+
+num = 123;
+console.log(num);
+
+const PI = 3.14;
+console.log(PI)
+
+num = 2.7;
+console.log(num);
+
+num = 16;
+console.log(num);
+
+let b = 10 !== 12;
+console.log(b);
+
+b = 10 === '10';
+console.log(b);
+
+let firstName = 'Anna';
+let middleName = 'Oleksandrivna';
+let lastName = 'Kochkina';
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+let a = 100;
+console.log(typeof a);
+
+let b1 = '100';
+console.log(typeof b1);
+
+let c = true;
+console.log(typeof c);
