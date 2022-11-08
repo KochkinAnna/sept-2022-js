40bfd864b955ddc9510b6065e48a48fd402604e2
KochkinAnna <kochkinaanichka@gmail.com>
2022-11-08 16:07:50 +0200

    ok

--- lesson3_js/main.js
+++ lesson3_js/main.js
@@ -81,9 +81,10 @@ switch (week) {
 //         Потрібно знайти та вивести максимальне число з цих двох.
 //         Також потрібно врахувати коли введені рівні числа.
 
-let number1 = prompt('Please enter your first number, I will show you the trick');
-let number2 = prompt('Please enter your second number');
-console.log(Math.max(number1,number2))
+let number1 = prompt('Please enter your first number, I will show you the trick')
+let number2 = prompt('Please enter your second number')
+console.log(Math.max(number1,number2));
+document.write(Math.max(number1,number2));
 
 //
 //     - є змінна х, яка може прийняти будь-яке значення (рядок, число, undefined, null і тд включно). Напишіть код який,
