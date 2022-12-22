ca8f5079b1657cf1e84c272b1344aeb9e1b11b0d
KochkinAnna <kochkinaanichka@gmail.com>
2022-12-23 01:19:36 +0200

    ...

--- mini_project_js/main-user-details-post.js
+++ mini_project_js/main-user-details-post.js
@@ -9,15 +9,30 @@ console.log(post);
 let parse = JSON.parse(post);
 console.log(parse);
 
-fetch('https://jsonplaceholder.typicode.com/users/${parse}/posts')
+fetch(`https://jsonplaceholder.typicode.com/posts/${parse.id}`)
     .then((response) => response.json())
     .then(value => {
-        for (const key in value) {
-            let infoPost = document.createElement('div');
-            infoPost.classList.add('div');
-            let h2 = document.createElement('h2');
-            h2.innerText = `${key}`;
-            infoPost.appendChild(h2);
-            document.body.append(infoPost);
-        }
+        let infoPost = document.createElement('div');
+        infoPost.classList.add('div');
+        let h2 = document.createElement('h2');
+        h2.innerText = `${value.id}. ${value.title}`;
+        let h3 = document.createElement('h3');
+        h3.innerText = `${value.body}`;
+        infoPost.append(h2, h3);
+        document.body.append(infoPost);
     });
+
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${parse.id}/comments`)
+    .then((response) => response.json())
+    .then(value => {
+        for (const valueElement of value) {
+            let commentsDiv = document.createElement('div');
+            let h4 = document.createElement('h4');
+            h4.innerText = `${valueElement.id}. ${valueElement.name}: `
+            let h5 = document.createElement('h5');
+            h5.innerText = `${valueElement.body}. ${valueElement.email}`
+            commentsDiv.append(h4, h5);
+            document.body.appendChild(commentsDiv)
+        }
+    })
